888ccb4d0b580ef595ae6425ba2ce5daf693875d
Trevor <trevorhauter99@gmail.com>
2024-09-11 22:42:01 -0400

    IT WORKS... WOOHOO

--- lua/gitportal/core.lua
+++ lua/gitportal/core.lua
@@ -2,14 +2,8 @@ local window_management = require('gitportal.window_management')
 local git_helpers = require('gitportal.git')
 
 
--- Map the function to a command for testing
-vim.api.nvim_create_user_command('HelloWorld', function()
-  window_management.open_window(git_helpers.get_base_git_directory())
-end, {})
-
-
 vim.api.nvim_create_user_command('Hey', function()
-  window_management.open_window(git_helpers.get_base_github_url())
+  window_management.open_window(git_helpers.get_git_url_for_current_file())
 end, {})
 
 
--- lua/gitportal/git.lua
+++ lua/gitportal/git.lua
@@ -4,7 +4,7 @@ local cli_utils = require("gitportal.cli_utils")
 local M = {}
 
 
-function M.get_base_git_directory()
+local function get_git_file_path()
   -- Gets a path of the file relative the the base git directory.
 
   -- Get the full path of the current file
@@ -14,21 +14,26 @@ function M.get_base_git_directory()
   -- Get the git root dir
   local git_root_dir = vim.fs.dirname(vim.fs.find(git_root_patterns, { upward = true })[1])
 
-  local last_dir = git_root_dir:match("([^/]+)$")
-  local git_path = current_file_path:sub(#git_root_dir + 1) -- Have to add one so we don't repeat last char
-  return last_dir .. git_path
+  local git_path = current_file_path:sub(#git_root_dir + 2) -- Have to add one so we don't repeat last char
+  return git_path
 end
 
 
---local function get_git_branch_name()
-  --git rev-parse --abbrev-ref HEAD
---end
+local function get_git_branch_name()
+  local branch_name = cli_utils.run_command("git rev-parse --abbrev-ref HEAD")
+  if branch_name then
+    branch_name = branch_name:gsub("\n", "")
+  else
+    branch_name = "FAILED"
+  end
+
+  return branch_name
+end
 
 
-function M.get_base_github_url()
+local function get_base_github_url()
   -- Get the base github url for a repo... 
-  -- Takes the messy output of git remote -v and returns just the base url! For this repo it would be...
-  -- https://github.com/trevorhauter/gitportal.nvim.git
+  -- Ex: https://github.com/trevorhauter/gitportal.nvim
   local url = cli_utils.run_command("git config --get remote.origin.url")
   if url then
     url = url:gsub("%.git\n$", "")
@@ -37,6 +42,21 @@ function M.get_base_github_url()
   end
 
   return url
+end
+
+
+function M.get_git_url_for_current_file()
+  -- Creates a url for the current file in github.
+  -- formula for url is 
+  -- https://github.com/trevorhauter/gitportal.nvim/blob/initial_setup/lua/gitportal/cli_utils.lua
+  -- remote url: https://github.com/trevorhauter/gitportal.nvim
+  -- blob: blob
+  -- branch_name: initial_setup
+  -- file_path: lua/gitportal/cli_utils.lua (Note doesn't include base dir, i.e. gitportal.nvim)
+  local remote_url = get_base_github_url()
+  local branch_name = get_git_branch_name()
+  local base_git_directory = get_git_file_path()
+  return remote_url .. "/blob/" .. branch_name .. "/" .. base_git_directory
 end
 
 
