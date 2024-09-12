ed26bbf7a9958ae44b9971dfaecdbde3dafe321c
Trevor <trevorhauter99@gmail.com>
2024-09-11 21:51:57 -0400

    Add func to get git remotes -v output

--- lua/gitlinks/core.lua
+++ lua/gitlinks/core.lua
@@ -1,21 +1,13 @@
 local window_management = require('gitlinks.window_management')
-
-local function getGitPath()
-  -- Gets a path of the file relative the the base git directory.
-  -- Get the full path of the current file
-  local current_file_path = vim.api.nvim_buf_get_name(0)
-
-  local git_root_patterns = { ".git" }
-  -- Get the git root dir
-  local git_root_dir = vim.fs.dirname(vim.fs.find(git_root_patterns, { upward = true })[1])
-
-  local last_dir = git_root_dir:match("([^/]+)$")
-  local git_path = current_file_path:sub(#git_root_dir + 1) -- Have to add one so we don't repeat last char
-  return last_dir .. git_path
-end
+local git_helpers = require('gitlinks.git')
 
 -- Map the function to a command for testing
 vim.api.nvim_create_user_command('HelloWorld', function()
-  window_management.open_hello_world_window(getGitPath())
+  window_management.open_hello_world_window(git_helpers.getGitPath())
 end, {})
+
+vim.api.nvim_create_user_command('Hey', function()
+  window_management.open_hello_world_window(git_helpers.get_git_remotes())
+end, {})
+
 
---
+++ lua/gitlinks/git.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+function M.getGitPath()
+  -- Gets a path of the file relative the the base git directory.
+  -- Get the full path of the current file
+  local current_file_path = vim.api.nvim_buf_get_name(0)
+
+  local git_root_patterns = { ".git" }
+  -- Get the git root dir
+  local git_root_dir = vim.fs.dirname(vim.fs.find(git_root_patterns, { upward = true })[1])
+
+  local last_dir = git_root_dir:match("([^/]+)$")
+  local git_path = current_file_path:sub(#git_root_dir + 1) -- Have to add one so we don't repeat last char
+  return last_dir .. git_path
+end
+
+
+function M.get_git_remotes()
+    local output = vim.fn.system("git remote -v")
+    if vim.v.shell_error ~= 0 then
+        -- Handle errors if the command fails
+        vim.notify("Failed to run git remote -v", vim.log.levels.ERROR)
+        return nil
+    end
+    return output:gsub("\n", "")
+end
+
+return M
