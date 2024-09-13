7946ab35da0f7537168d0f2cfd771542be235707
Trevor <trevorhauter99@gmail.com>
2024-09-12 23:56:21 -0400

    Update command

--- lua/gitportal/core.lua
+++ lua/gitportal/core.lua
@@ -2,7 +2,7 @@ local ui = require('gitportal.ui')
 local git_helpers = require('gitportal.git')
 
 
-vim.api.nvim_create_user_command('Hey', function()
+vim.api.nvim_create_user_command('Gplink', function()
   ui.open_window(git_helpers.get_git_url_for_current_file())
 end, {})
 
