3cca30cd16d2ee0629fa51572582774590f50bb5
Trevor <trevorhauter99@gmail.com>
2024-09-12 23:24:03 -0400

    Update name

--- lua/gitportal/core.lua
+++ lua/gitportal/core.lua
@@ -1,9 +1,9 @@
-local window_management = require('gitportal.ui')
+local ui = require('gitportal.ui')
 local git_helpers = require('gitportal.git')
 
 
 vim.api.nvim_create_user_command('Hey', function()
-  window_management.open_window(git_helpers.get_git_url_for_current_file())
+  ui.open_window(git_helpers.get_git_url_for_current_file())
 end, {})
 
 
