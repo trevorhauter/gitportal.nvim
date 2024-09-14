26515a1dd35a21a829270f0b89c9414c8c831a4c
Trevor <trevorhauter99@gmail.com>
2024-09-13 21:33:55 -0400

    Rename cli, add method to open url automatically

--- lua/gitportal/core.lua
+++ lua/gitportal/core.lua
@@ -1,9 +1,10 @@
 local ui = require('gitportal.ui')
+local cli = require('gitportal.cli')
 local git_helpers = require('gitportal.git')
 
 
 vim.api.nvim_create_user_command('Gplink', function()
-  ui.open_window(git_helpers.get_git_url_for_current_file())
+  cli.open_link_in_browser(git_helpers.get_git_url_for_current_file())
 end, {})
 
 
