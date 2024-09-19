4ee6fd4dc228ab5bbd60e805f93c3c9599f2017a
Trevor <trevorhauter99@gmail.com>
2024-09-18 21:28:32 -0400

    Update function to get visual mode selections in real time

--- lua/gitportal/vi_utils.lua
+++ lua/gitportal/vi_utils.lua
@@ -2,9 +2,14 @@
 local M = {}
 
 function M.get_visual_selection_lines()
-  -- Get the start and end line numbers of the visual selection
-  local start_line = vim.fn.getpos("'<")[2]
-  local end_line = vim.fn.getpos("'>")[2]
+  -- Get the start and end line numbers of the current visual selection
+  local start_line = vim.fn.line("v") -- Get visual start line
+  local end_line = vim.fn.line(".") -- Get current cursor line
+
+  -- Make sure start_line is less than end_line
+  if start_line > end_line then
+    start_line, end_line = end_line, start_line
+  end
 
   return start_line, end_line
 end
