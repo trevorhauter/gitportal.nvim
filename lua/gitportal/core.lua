ce4aa4bca2c79d588ab33dd561f30b2d08e961de
Trevor <trevorhauter99@gmail.com>
2024-09-22 16:16:46 -0400

    Add support for link ingestion

--- lua/gitportal/core.lua
+++ lua/gitportal/core.lua
@@ -4,10 +4,17 @@ local git_helpers = require('gitportal.git')
 local M = {}
 
 
-function M.open_file()
+function M.open_file_in_browser()
   local git_url = git_helpers.get_git_url_for_current_file()
   if git_url ~= nil then
     cli.open_link_in_browser(git_url)
+  end
+end
+
+
+function M.open_file_in_neovim(link)
+  if link ~= nil then
+    git_helpers.open_file_from_git_url(link)
   end
 end
 
