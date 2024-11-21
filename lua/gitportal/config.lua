40b7342b0151efec0766d9293e17d9a9c94962c8
Trevor <trevorhauter99@gmail.com>
2024-11-21 18:27:46 -0500

    Add ability to specify browser command in config

--- lua/gitportal/config.lua
+++ lua/gitportal/config.lua
@@ -4,6 +4,7 @@ local M = {}
 local default = {
     always_include_current_line = false,
     switch_branch_or_commit_upon_ingestion = "always", -- Can be "always", "ask_first", or "never"
+    browser_command = nil, -- String of the command used on command line to open link in browser
 }
 
 M.options = default
