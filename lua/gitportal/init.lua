41190a4ff91e62bc8e644d126a097fc87440177a
Trevor Hauter <74263491+trevorhauter@users.noreply.github.com>
2024-11-21 14:42:07 +0000

    Add style guide, reformat files, add github workflows (#45)
    * Add StyLua style config
    
    * Reformat files
    
    * Add github workflows to enforce formatting and run unittests
    
    Revert "Add improper styling to check if github action works"
    
    This reverts commit 255df536de8e20bb4cca76d4c2a41fb8fba2dc8f.
    
    Add experimental lua test. Does this even work?
    
    Fix yml file
    
    Fix yml file fr?
    
    Add missing step
    
    Fix issue
    
    Fix issue again. I'm going to squash this.
    
    Update path of run test call
    
    Update path
    
    Hopefully fix path issue
    
    Update installation path of luaunit
    
    Update installation path of luaunit with name
    
    Fix tests. Update github actions
    
    Add back local flag
    
    Please work
    
    Update workflow so it works
    
    Add debug statements
    
    Run at same time
    
    Update path
    
    Revert "Update path"
    
    This reverts commit 72820dd0ea70912e0cb4c8acd76c3a0d5ad87080.
    
    Add debug statement
    
    Specify lua version
    
    Moar debug statemetns
    
    Fix debug statement
    
    Fix debug statemetn... again
    
    Update command? What's going on here?
    
    Finally fix path
    
    update action
    
    add additional debug statement
    
    update path
    
    Update path
    
    Update test to break
    
    Revert "Update test to break"
    
    This reverts commit a5ca1fb91a036a43dc9c522c3662ffcf9b9b6045.

--- lua/gitportal/init.lua
+++ lua/gitportal/init.lua
@@ -1,29 +1,27 @@
-local cli = require('gitportal.cli')
-local git_helpers = require('gitportal.git')
-local config = require('gitportal.config')
+local cli = require("gitportal.cli")
+local config = require("gitportal.config")
+local git_helpers = require("gitportal.git")
 
 local M = {}
 
 -- SETUP
 function M.setup(options)
-  config.setup(options)
+    config.setup(options)
 end
 
 -- CORE FUNCTIONS
 function M.open_file_in_browser()
-  local git_url = git_helpers.get_git_url_for_current_file()
-  if git_url ~= nil then
-    cli.open_link_in_browser(git_url)
-  end
+    local git_url = git_helpers.get_git_url_for_current_file()
+    if git_url ~= nil then
+        cli.open_link_in_browser(git_url)
+    end
 end
-
 
 function M.open_file_in_neovim()
-  local url = vim.fn.input("Github link > ")
-  if url ~= nil then
-    git_helpers.open_file_from_git_url(url)
-  end
+    local url = vim.fn.input("Github link > ")
+    if url ~= nil then
+        git_helpers.open_file_from_git_url(url)
+    end
 end
-
 
 return M
