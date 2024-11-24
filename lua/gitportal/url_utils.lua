7b93f04789af8244c20149b1dd4b71979b213fca
Trevor Hauter <74263491+trevorhauter@users.noreply.github.com>
2024-11-24 05:55:55 +0000

    Add support for gitlab (#52)
    Add support for gitlab, refactor tests as well as add more coverage.

--- lua/gitportal/url_utils.lua
+++ lua/gitportal/url_utils.lua
@@ -1,16 +1,18 @@
+local cli = require("gitportal.cli")
+
 local M = {}
 
-function M.parse_githost_url(url)
-    -- Given a githost URL, parse all of the info we care about out of it!
-    local repo, branch_or_commit, file_path = url:match("github.com/[^/]+/([^/]+)/blob/([^/]+)/([^\n#]+)")
-    -- check for line numbers
+local function parse_line_range(url)
+    -- Given a url, parse the optional line range from the end of the URL itself.
+    -- These may looks like #L5-L11 or #L5-11, or may not be present at all.
     local start_line = nil
     local end_line = nil
 
-    if string.find(url, "#", 0, true) ~= nil then
+    -- TODO: Is this robust enough?
+    if string.find(url, "#L", 0, true) ~= nil then
         start_line = url:match("#L(%d+)")
         if string.find(url, "-", 0, true) ~= nil then
-            end_line = url:match("%-L(%d+)$")
+            end_line = url:match("#L%d+%-L?(%d+)$")
         end
     end
 
@@ -21,6 +23,36 @@ function M.parse_githost_url(url)
         start_line = tonumber(start_line)
         end_line = tonumber(end_line)
     end
+    return start_line, end_line
+end
+
+local function parse_github_url(url)
+    -- a GitHub url may appear as follows... (Check tests for more variants)
+    -- https://github.com/trevorhauter/gitportal.nvim/blob/main/lua/gitportal/cli.lua#L45-L55
+    local repo, branch_or_commit, file_path = url:match("github.com/[^/]+/([^/]+)/blob/([^/]+)/([^\n#]+)")
+    return repo, branch_or_commit, file_path
+end
+
+local function parse_gitlab_url(url)
+    -- a GitLab url may appear as follows... (Check tests for more variants)
+    -- https://gitlab.com/gitportal/gitlab-test/-/blob/master/public/index.html?ref_type=heads#L5-11
+    local repo, branch_or_commit, file_path = url:match("gitlab.com/[^/]+/([^/]+)/%-?/?blob/([^/]+)/([^\n%?#]+)")
+    return repo, branch_or_commit, file_path
+end
+
+function M.parse_githost_url(url)
+    local githost_parse_func
+
+    if string.find(url, "github") ~= nil then
+        githost_parse_func = parse_github_url
+    elseif string.find(url, "gitlab") ~= nil then
+        githost_parse_func = parse_gitlab_url
+    else
+        cli.log_error("Could not determine valid githost from url!")
+    end
+
+    local repo, branch_or_commit, file_path = githost_parse_func(url)
+    local start_line, end_line = parse_line_range(url)
 
     return {
         repo = repo,
